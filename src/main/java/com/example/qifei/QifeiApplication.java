922bd03dcddbb99ef259bac9a98fe9dcce51d318
xinjie.li <xinjie.li@ipinyou.com>
2021-09-16 09:23:12 +0800

    起飞

---
+++ src/main/java/com/example/qifei/QifeiApplication.java
@@ -0,0 +1,13 @@
+package com.example.qifei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QifeiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QifeiApplication.class, args);
+    }
+
+}
